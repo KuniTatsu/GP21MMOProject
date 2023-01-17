62a2cdc00e31078d8a91d91c0d0961124be0e67d
KuniTatsu <kunitatsuvantan@gmail.com>
2023-01-17 17:31:16 +0900

    [T] 実装例を追加

---
+++ DxGameClient/program/game/Inventory.cpp
@@ -0,0 +1,152 @@
+/*
+#include "Inventory.h"
+#include"DxLib.h"
+#include"../../library/t2klib.h"
+#include"../../support/Support.h"
+#include"Item.h"
+#include"equipItem.h"
+#include"../GameManager.h"
+#include"../Player.h"
+#include"../SoundManager.h"
+
+extern GameManager* gManager;
+Inventory::Inventory(int MyInventoryNum)
+{
+	cursorGh = gManager->LoadGraphEx("graphics/menuCursor.png");
+	selectItemBackGh = gManager->LoadGraphEx("graphics/selectItemBack.png");
+	myInventoryNum = MyInventoryNum + 1;
+}
+
+Inventory::~Inventory()
+{
+}
+
+void Inventory::AddInventory(Item* item)
+{
+	if (inventoryList.size() == 10)return;
+	inventoryList.emplace_back(item);
+	itemNum++;
+
+}
+
+void Inventory::CursorMove()
+{
+	if (t2k::Input::isKeyDownTrigger(t2k::Input::KEYBORD_UP)) {
+		itemNum = GetItemCount();
+		gManager->sound->System_Play(gManager->sound->system_move);
+		selectCursor = (selectCursor + (itemNum - 1)) % itemNum;
+	}
+	else if (t2k::Input::isKeyDownTrigger(t2k::Input::KEYBORD_DOWN)) {
+		itemNum = GetItemCount();
+		gManager->sound->System_Play(gManager->sound->system_move);
+		selectCursor = (selectCursor + 1) % itemNum;
+	}
+}
+//インベントリのカーソル位置を一番上にリセットする関数
+void Inventory::CursorReset()
+{
+	selectCursor = 0;
+}
+//インベントリ内のアイテム名を描画する関数
+void Inventory::DrawInventory(int x, int y)
+{
+	int i = 0;
+
+	
+
+for (auto item : inventoryList) {
+	//装備アイテムなら
+	if (item->getItemData(1) >= 2) {
+		equipItem* eItem = static_cast<equipItem*>(item);
+		//装備中のアイテムなら'E'を頭に描画する
+		if (eItem->GetIsEquiped())DrawStringEx(x + 40, y + 10 + 30 * i, -1, "[E]");
+		//アイテム名の描画
+		DrawStringEx(x + 80, y + 10 + 30 * i, -1, "%s", item->getItemName().c_str());
+	}
+	else {
+		//アイテム名の描画
+		DrawStringEx(x + 80, y + 10 + 30 * i, -1, "%s", item->getItemName().c_str());
+	}
+	++i;
+}
+//選択中アイテムの横にカーソルを描画する
+DrawRotaGraph(x + 30, y + 20 + selectCursor * 30, 0.7, 0, cursorGh, true);
+SetDrawBlendMode(DX_BLENDMODE_ALPHA, 30);
+DrawRotaGraph(x + 220, y + 20 + selectCursor * 30, 1, 0, selectItemBackGh, true);
+SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 255);
+}
+//アイテムの説明を描画する関数
+void Inventory::DrawItemDesc(int x, int y)
+{
+	if (inventoryList.empty())return;
+	auto itr = inventoryList.begin();
+	for (int i = 0; i < selectCursor; ++i) {
+		if (itr == inventoryList.end()) {
+			break;
+		}
+		itr++;
+	}
+	(*itr)->DrawItemStringData(x, y);
+	//inventory[selectCursor]->DrawItemData(x, y);
+}
+
+void Inventory::DrawEquipItemStatus(const int x, const int y)
+{
+	if (inventoryList.empty())return;
+
+	auto itr = inventoryList.begin();
+	for (int i = 0; i < selectCursor; ++i) {
+		if (itr == inventoryList.end()) {
+			break;
+		}
+		itr++;
+	}
+	//装備アイテムならデータを取得して描画する
+	if ((*itr)->getItemData(1) >= 2) {
+		auto item = static_cast<equipItem*>((*itr));
+		item->DrawEquipItemStatus(x, y, item->GetSubId());
+	}
+
+}
+
+void Inventory::DrawNeedCoin(int x, int y)
+{
+	if (inventoryList.empty())return;
+	auto itr = inventoryList.begin();
+	for (int i = 0; i < selectCursor; ++i) {
+		if (itr == inventoryList.end()) {
+			break;
+		}
+		itr++;
+	}
+	int needCoin = 0;
+	if ((*itr)->getItemData(1) >= 2) {
+		auto item = static_cast<equipItem*>((*itr));
+		needCoin = item->getItemData(10);
+	}
+	else {
+		needCoin = (*itr)->getItemData(5);
+	}
+	//文字サイズ変更
+	SetFontSize(25);
+	DrawStringEx(x + 10, y + 10, -1, "購入必要コイン:%d", needCoin);
+	DrawStringEx(x + 10, y + 60, -1, "所持コイン:%d", gManager->player->GetHaveCoin());
+	//文字サイズ変更
+	SetFontSize(16);
+}
+
+int Inventory::GetCursorNum()
+{
+	return selectCursor;
+}
+
+void Inventory::SetCursorNum(int MoveNum)
+{
+	if (selectCursor != inventoryList.size())return;
+	selectCursor += MoveNum;
+	if (selectCursor < 0)selectCursor = 0;
+
+}
+
+
+*/
---
+++ DxGameClient/program/game/Inventory.h
@@ -0,0 +1,82 @@
+#pragma once
+
+/*
+///*****Description*****
+///インベントリに関するクラス
+///プレイヤーの所有するアイテムの情報を保持する
+/// インベントリ内のアイテムの処理も行う
+///*********************
+#pragma once
+#include<list>
+#include<memory>
+class Item;
+
+class Inventory
+{
+public:
+	Inventory(int MyInventoryNum);
+	~Inventory();
+
+	//std::shared_ptr<Item>inventory[10] = { nullptr,nullptr ,nullptr ,nullptr ,nullptr ,nullptr ,nullptr ,nullptr ,nullptr ,nullptr };
+
+	//内部にItemを10個持つlist
+	std::list<Item*> inventoryList;
+	//std::list<std::shared_ptr<Item>> inventorySharedList;
+
+	//インベントリ内のアイテム数を取得する関数
+	inline int GetItemCount() {
+		return inventoryList.size();
+	}
+	//インベントリにアイテムを追加する関数
+	void AddInventory(Item* item);
+
+	//void AddSharedInventory(std::shared_ptr<Item>item);
+	//
+	//カーソルを上下に動かす関数
+	void CursorMove();
+	//カーソルを一番上に戻す関数
+	void CursorReset();
+	//インベントリ内のアイテム名を描画する関数
+	void DrawInventory(const int x, const int y);
+	//カーソルで選択中のアイテムの説明を描画する関数
+	void DrawItemDesc(const int x, const int y);
+	//カーソルで選択中の装備アイテムのステータスを描画する関数
+	void DrawEquipItemStatus(const int x, const int y);
+
+	//ショップインベントリ用 購入額の表示
+	void DrawNeedCoin(int x, int y);
+
+	//カーソルの位置を取得する関数
+	int GetCursorNum();
+
+	//カーソルの位置を変更する関数 type:0->加算移動,1->指定位置移動
+	void SetCursorNum(const int MoveNum);
+
+	//インベントリ番号を取得する関数
+	inline int GetInventoryNum() {
+		return myInventoryNum;
+	}
+
+	inline void SetItemNum(const int num) {
+		itemNum += num;
+	}
+
+private:
+	//選択中のアイテムを指すカーソルの位置
+	int selectCursor = 0;
+	//カーソルgh
+	int cursorGh = 0;
+	//選択中アイテムの背景
+	int selectItemBackGh = 0;
+	//インベントリ内のアイテム数
+	int itemNum = 0;
+
+	//インベントリ番号
+	int myInventoryNum = 0;
+
+
+};
+
+
+
+*/
