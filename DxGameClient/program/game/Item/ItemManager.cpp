fa6d047087da6a9fcb8891ad0995aacb8b5cc51b
YonahaYuta <88570219+YonahaYuta@users.noreply.github.com>
2023-01-26 19:41:04 +0900

    [T]Itemcsv読み込み。item情報取得

--- DxGameClient/program/game/Item/ItemManager.cpp
+++ DxGameClient/program/game/Item/ItemManager.cpp
@@ -2,8 +2,31 @@
 
 ItemManager::ItemManager()
 {
+	fieldDropItems.clear();
+	itemMaster.resize(static_cast<int>(ITEMTYPE::MAX));
 }
 
 ItemManager::~ItemManager()
 {
 }
+
+void ItemManager::LoadItemCsv(std::string pass, std::vector< std::vector<int>>& putInVector, int type) {
+	//引数のURLで指定されたCSVファイルの中身を文字列として取得する
+	auto loadCsv = tnl::LoadCsv(pass);
+
+
+}
+
+
+void ItemManager::update(float deltatime) {
+
+}
+
+
+
+void ItemManager::SpawnItem() {
+	
+}
+
+
+
--- DxGameClient/program/game/Item/ItemManager.h
+++ DxGameClient/program/game/Item/ItemManager.h
@@ -1,13 +1,44 @@
 #pragma once
+#include<vector>
+#include<list>
+#include<memory>
+#include"../../dxlib_ext/dxlib_ext.h"
+/*
+アイテムのマスター情報を持つ　マスターはcsvから読み込み
+外部からアイテムを生成する為に呼ばれる関数をもつが、どのアイテムが選ばれるかはSpawnManagerにやってもらう
+選ばれたアイテムを生成し、返り値で返す関数を作り、外部に作られたアイテムを返す
+敵からのドロップだった場合は、ItemManagerが持つフィールドドロップリストに入れる
 
+*/
+class Item;
 
 class ItemManager
 {
 public:
 	ItemManager();
 	~ItemManager();
+	//外部からアイテムを作りたいときに呼ぶ関数
+	void SpawnItem();
+
+	void update(float deltatime);
+
+	enum class ITEMTYPE {
+		CONSUME,
+		EQUIP_WEAPON,
+		EQUIP_ARMOR,
+		MATERIAL,
+		MAX
+	};
 
 private:
+	std::shared_ptr<Item> CreateItem(int type, tnl::Vector3& spawnPos);
 
+	void LoadItemCsv(std::string pass, std::vector< std::vector<int>>& putInVector, int type);
+
+private:
+	std::list <std::shared_ptr<Item>>fieldDropItems;
+
+	//アイテムマスター
+	std::vector < std::vector<std::shared_ptr<Item>>>itemMaster;
 };
 
